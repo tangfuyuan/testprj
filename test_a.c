75bbf1ae39799a4086986a31d0307342699c3593
edaplayer <edaplayer@163.com>
2016-04-14 15:48:31 +0800

    delete test.c by richard

--- test_a.c
+++ test_a.c
@@ -1,11 +1,9 @@
 /*************************************************************************
-    > File Name: test.c
+    > File Name: test_a.c
     > Author: Richard
     > Mail:   edaplayer@163.com 
-    > Created Time: Thu 14 Apr 2016 03:11:41 PM CST
+    > Created Time: Thu 14 Apr 2016 03:32:11 PM CST
  ************************************************************************/
 
 #include<stdio.h>
-sdff
-fist
-
+dfdf
